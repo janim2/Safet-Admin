aaa1049704f18bab872d0701be7d44c61709606d
Jesse Anim <iamjesse75@gmail.com>
2019-12-25 19:10:44 +0000

    update school images to database

--- app/src/main/java/com/example/sergeherkul/bustrackeradmin/Edit_Images.java
+++ app/src/main/java/com/example/sergeherkul/bustrackeradmin/Edit_Images.java
@@ -19,12 +19,16 @@ import android.widget.Toast;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.OnProgressListener;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -70,6 +74,8 @@ public class Edit_Images extends AppCompatActivity {
 
         storage = FirebaseStorage.getInstance();
         storageReference = storage.getReference();
+
+        Fetch_Image_One();
 
         image_one_cardView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -143,8 +149,9 @@ public class Edit_Images extends AppCompatActivity {
                             .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
                                 @Override
                                 public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
-//                                    double progress = (100.0*taskSnapshot.getBytesTransferred()/taskSnapshot
-//                                            .getTotalByteCount());
+                                    double progress = (100.0*taskSnapshot.getBytesTransferred()/taskSnapshot
+                                            .getTotalByteCount());
+                                    Toast.makeText(Edit_Images.this,"Image one: " + progress+""+"%",Toast.LENGTH_LONG).show();
 //                                    progressDialog.setMessage("Uploaded "+(int)progress+"%");
                                 }
                             });
@@ -183,8 +190,9 @@ public class Edit_Images extends AppCompatActivity {
                                 .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
                                     @Override
                                     public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
-//                                    double progress = (100.0*taskSnapshot.getBytesTransferred()/taskSnapshot
-//                                            .getTotalByteCount());
+                                    double progress = (100.0*taskSnapshot.getBytesTransferred()/taskSnapshot
+                                            .getTotalByteCount());
+                                    Toast.makeText(Edit_Images.this,"Image two: " + progress+""+"%",Toast.LENGTH_LONG).show();
 //                                    progressDialog.setMessage("Uploaded "+(int)progress+"%");
                                     }
                                 });
@@ -227,8 +235,9 @@ public class Edit_Images extends AppCompatActivity {
                                 .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
                                     @Override
                                     public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
-//                                    double progress = (100.0*taskSnapshot.getBytesTransferred()/taskSnapshot
-//                                            .getTotalByteCount());
+                                    double progress = (100.0*taskSnapshot.getBytesTransferred()/taskSnapshot
+                                            .getTotalByteCount());
+                                    Toast.makeText(Edit_Images.this,"Image three: " + progress+""+"%",Toast.LENGTH_LONG).show();
 //                                    progressDialog.setMessage("Uploaded "+(int)progress+"%");
                                     }
                                 });
@@ -242,6 +251,114 @@ public class Edit_Images extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    private void Fetch_Image_One() {
+        try {
+            DatabaseReference image_one = FirebaseDatabase.getInstance().getReference("images").child("school_images").child(school_code).child("image_one");
+            image_one.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    if(dataSnapshot.exists()){
+                        for(DataSnapshot child : dataSnapshot.getChildren()){
+                            if(child.getKey().equals("image")){
+                                String image_one_image = child.getValue().toString();
+                                if(image_one_image.equals("")){
+                                }else{
+                                    Picasso.with(Edit_Images.this).load(Uri.parse(image_one_image)).into(imageView_one);
+                                }
+                            }
+
+                            else{
+//                            Toast.makeText(getActivity(),"Couldn't fetch posts",Toast.LENGTH_LONG).show();
+
+                            }
+                        }
+                        Fetch_Image_Two();
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+                    Toast.makeText(Edit_Images.this,"Cancelled",Toast.LENGTH_LONG).show();
+
+                }
+            });
+        }catch (NullPointerException e){
+
+        }
+    }
+
+    private void Fetch_Image_Two() {
+        try {
+            DatabaseReference image_two = FirebaseDatabase.getInstance().getReference("images").child("school_images").child(school_code).child("image_two");
+            image_two.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    if(dataSnapshot.exists()){
+                        for(DataSnapshot child : dataSnapshot.getChildren()){
+                            if(child.getKey().equals("image")){
+                                String image_two_image = child.getValue().toString();
+                                if(image_two_image.equals("")){
+                                }else{
+                                    Picasso.with(Edit_Images.this).load(Uri.parse(image_two_image)).into(imageView_two);
+                                }
+                            }
+
+                            else{
+//                            Toast.makeText(getActivity(),"Couldn't fetch posts",Toast.LENGTH_LONG).show();
+
+                            }
+                        }
+                        Fetch_Image_Three();
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+                    Toast.makeText(Edit_Images.this,"Cancelled",Toast.LENGTH_LONG).show();
+
+                }
+            });
+        }catch (NullPointerException e){
+
+        }
+    }
+
+    private void Fetch_Image_Three() {
+        try {
+            DatabaseReference image_three = FirebaseDatabase.getInstance().getReference("images").child("school_images").child(school_code).child("image_three");
+            image_three.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    if(dataSnapshot.exists()){
+                        for(DataSnapshot child : dataSnapshot.getChildren()){
+                            if(child.getKey().equals("image")){
+                                String image_three_image = child.getValue().toString();
+                                if(image_three_image.equals("")){
+                                }else{
+                                    Picasso.with(Edit_Images.this).load(Uri.parse(image_three_image)).into(imageView_three);
+                                }
+                            }
+
+                            else{
+//                            Toast.makeText(getActivity(),"Couldn't fetch posts",Toast.LENGTH_LONG).show();
+
+                            }
+                        }
+                        Fetch_Image_Three();
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+                    Toast.makeText(Edit_Images.this,"Cancelled",Toast.LENGTH_LONG).show();
+
+                }
+            });
+        }catch (NullPointerException e){
+
+        }
     }
 
     @Override
